9255aeb078f937a64e736e04836c392f8f873e5e
George Coleman <george.coleman@justpinch.com>
2017-11-04 14:50:35 +0100

    recipe list first commit

---
+++ DinnerSpinner/DinnerSpinner/UI/CommonViews/DSRatingView.swift
@@ -0,0 +1,22 @@
+//
+//  DSRatingView.swift
+//  DinnerSpinner
+//
+//  Created by George Coleman on 04/11/2017.
+//  Copyright © 2017 Digithio. All rights reserved.
+//
+
+import UIKit
+import Cosmos
+
+class DSRatingView: CosmosView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
